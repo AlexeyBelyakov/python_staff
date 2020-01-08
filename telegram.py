69da78d67e46fb479a3c422f166ff99e6ae0dfc4
AlexeyBelyakov <asbelyak@gmail.com>
2020-01-08 23:37:58 +0300

    companion

---
+++ telegram.py
@@ -0,0 +1,44 @@
+# import pytelegrambotapi
+import telebot
+import os
+
+# питон хуйон
+# люба хуюба
+# лёша хуёша
+#
+#
+#
+
+key_msg = "хуй"
+short_key_msg = "ху"
+
+
+def make_hui(str):
+    words = str.split()
+    if len(words) == 0:
+        return key_msg
+    word_to_return = words[-1]
+    if len(word_to_return) < 3:
+        return key_msg
+    vowels = "АЕЁИОУЫЭЮЯ".lower()
+    word_to_return = word_to_return.lower()
+    if word_to_return[-3] in vowels:
+        return short_key_msg + word_to_return[-3:]
+    else:
+        return key_msg + word_to_return[-2:]
+
+
+if __name__ == '__main__':
+    print(os.environ['BENDER_TOKEN'])
+    bot = telebot.TeleBot(os.environ['BENDER_TOKEN'])
+
+    @bot.message_handler(commands=['start'])
+    def start_message(message):
+        bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+
+    @bot.message_handler(content_types=['text'])
+    def send_text(message):
+        print(message.chat.id, message.text)
+        bot.send_message(message.chat.id, make_hui(message.text))
+
+    bot.polling()
